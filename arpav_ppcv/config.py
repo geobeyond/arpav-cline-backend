d542842a7a492aac03f5e0ddfea0221de3e5486c
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2024-12-02 11:53:48 +0000

    Added config option for setting timeout of ncss-related requests
    Also set a default value of 30s (the previous value was 5s, which is httpx's default)

--- arpav_ppcv/config.py
+++ arpav_ppcv/config.py
@@ -47,6 +47,7 @@ class ThreddsServerSettings(pydantic.BaseModel):
     base_url: str = "http://localhost:8080/thredds"
     wms_service_url_fragment: str = "wms"
     netcdf_subset_service_url_fragment: str = "ncss/grid"  # noqa
+    netcdf_subset_service_timeout_seconds: int = 30  # noqa
     opendap_service_url_fragment: str = "dodsC"  # noqa
     uncertainty_visualization_scale_range: tuple[float, float] = pydantic.Field(
         default=(0, 9)
